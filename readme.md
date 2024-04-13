ab27fc2065c869f275ac9ee514682a5277594c10
Melissa Autumn <all@melissaautumn.com>
2024-04-13 11:11:07 -0700

    Lots of adjustments for pyproject / making it buildable with pyinstaller.

--- readme.md
+++ readme.md
@@ -6,4 +6,10 @@ Just playing around with Pyside6, nothing to see here!
 
 ## Installation
 
-Create a virtual env that allows access to system packages, install `requirements.txt`, and ensure you have kirigami and Pyside6 installed from your system package manager. (Yeah that's annoying I know.)
+Create a virtual env that allows access to system packages, run `pip install -e .`, and ensure you have kirigami and Pyside6 installed from your system package manager. (Yeah that's annoying I know.)
+
+## Building
+
+Install any build deps by running `pip install -e .[install]` and then run `pyinstaller --add-data ./src/untitledmusicplayer/qml:qml ./src/untitledmusicplayer/app.py` to build the app.
+
+It will then appear in `dist/app/app` (or `dist/app/app.exe` on windows.)
--- src/untitledmusicplayer/app.py
+++ src/untitledmusicplayer/app.py
@@ -1,17 +1,21 @@
 import json
+import pathlib
 import random
+import signal
 import string
 import sys, os
 
 from PySide6 import QtCore
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import QUrl
+from PySide6.QtGui import QGuiApplication, QPalette, QColor
 from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
 from PySide6.QtQuickControls2 import QQuickStyle
 from PySide6.QtWidgets import QApplication
 from dotenv import load_dotenv
+from platformdirs import user_data_dir, user_cache_dir
 
-from audio.api.jellyfin import JellyfinAPI
-from models.album import Album, ItemRef, Image
+from untitledmusicplayer.audio.api.jellyfin import JellyfinAPI
+from untitledmusicplayer.models.album import Album, ItemRef, Image
 
 class QAlbum(QtCore.QObject):
     album : Album
@@ -21,7 +25,7 @@ class QAlbum(QtCore.QObject):
 
     @QtCore.Property('QVariant', constant=True)
     def model(self):
-        print(self.album.model_dump())
+        #print(self.album.model_dump())
         return self.album.model_dump()
 
 
@@ -39,23 +43,33 @@ def qt_message_handler(mode, context, message):
     print("%s: %s (%s:%d, %s)" % (mode, message, context.file, context.line, context.file))
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
+    """Needed to get proper KDE style outside of Plasma"""
+    if not os.environ.get("QT_QUICK_CONTROLS_STYLE"):
+        os.environ["QT_QUICK_CONTROLS_STYLE"] = "org.kde.desktop"
+
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    data_path = user_data_dir('untitledmusicapp', 'melissaautumn')
+    cache_path = user_cache_dir('untitledmusicapp', 'melissaautumn')
+    album_cache_path = pathlib.Path(f'{cache_path}/tmp/albums.json')
+
+    # Ensure the path is created
+    album_cache_path.parent.mkdir(parents=True, exist_ok=True)
 
     QtCore.qInstallMessageHandler(qt_message_handler)
     app = QApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
-    """Needed to get proper KDE style outside of Plasma"""
-    if not os.environ.get("QT_QUICK_CONTROLS_STYLE"):
-        os.environ["QT_QUICK_CONTROLS_STYLE"] = "org.kde.desktop"
+    """Needed to close the app with Ctrl+C"""
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     jellyfin_api = JellyfinAPI(os.getenv('JELLYFIN_HOST'), os.getenv('JELLYFIN_PORT'), os.getenv('JELLYFIN_USE_SSL').lower() == 'true', os.getenv('JELLYFIN_USER'), os.getenv('JELLYFIN_ACCESS_TOKEN'))
 
     albums_with_images = []
 
     try:
-        with open('tmp/albums.json', 'r') as fh:
+        with open(album_cache_path, 'r') as fh:
             albums_with_images = json.loads(fh.read())
     except FileNotFoundError:
         pass
@@ -71,7 +85,6 @@ if __name__ == "__main__":
 
             jellyfin_api.auth_confirm(response.get('Secret'))
 
-
         me = jellyfin_api.get_me()
         views = jellyfin_api.get_views()
 
@@ -86,9 +99,9 @@ if __name__ == "__main__":
 
         print(albums)
 
-        with open('tmp/albums.json', 'w') as fh:
-            fh.write(json.dumps(albums_with_images))
 
+        with open(album_cache_path, 'w') as fh:
+            fh.write(json.dumps(albums_with_images))
 
     api_url = jellyfin_api._api_url()
     print("API URL >> ", api_url)
@@ -120,7 +133,7 @@ if __name__ == "__main__":
     engine.rootContext().setContextProperty('api_url', api_url)
 
     print("Loading main")
-    engine.load('qml/main.qml')
+    engine.load(QUrl(f'file://{base_path}/qml/main.qml'))
 
     if not engine.rootObjects():
         sys.exit(-1)
@@ -128,3 +141,6 @@ if __name__ == "__main__":
     exit_code = app.exec()
     del engine
     sys.exit(exit_code)
+
+if __name__ == "__main__":
+    main()
